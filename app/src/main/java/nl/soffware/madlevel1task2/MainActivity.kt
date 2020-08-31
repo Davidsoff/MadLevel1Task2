106bac2a6cb4cfeab265ca51e1130f4fb95d232c
David Soff <david@soff.nl>
2020-09-01 00:13:10 +0200

    Finish conjecture trainer

--- app/src/main/java/nl/soffware/madlevel1task2/MainActivity.kt
+++ app/src/main/java/nl/soffware/madlevel1task2/MainActivity.kt
@@ -2,6 +2,12 @@ package nl.soffware.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import nl.soffware.madlevel1task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +17,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initialize()
     }
+
+    private fun initialize() {
+        binding.button.setOnClickListener() { handleSubmit()  }
+    }
+
+    private fun handleSubmit() {
+        val correct = checkAnswers()
+        Toast.makeText(this, getString(R.string.results, correct), Toast.LENGTH_LONG).show()
+    }
+    private fun checkAnswers(): Int {
+        var correct = 0
+        val t = getString(R.string.truth)
+        val f = getString(R.string.untrue)
+        if(binding.case1.selectedItem.toString() == t) correct += 1
+        if(binding.case2.selectedItem.toString() == f) correct += 1
+        if(binding.case3.selectedItem.toString() == f) correct += 1
+        if(binding.case4.selectedItem.toString() == f) correct += 1
+        return correct
+    }
+
+
 }
